28cfd9fc82e74d9460bfcf7e7de7a957a409f032
IKA-Syrian <computertk2018@gmail.com>
2026-01-09 19:21:11 +0300

    Fix command syntax and improve folder handling in stitch process

--- stitch-system.js
+++ stitch-system.js
@@ -18,7 +18,7 @@ module.exports = (client) => {
             const height = interaction.options.getInteger('height')
             const format = interaction.options.getString('format')
             const width = interaction.options.getInteger('width')
-            
+
             let fileID
             if (link.split('?id=').length == 2) {
                 fileID = link.split('?id=')[1]
@@ -36,7 +36,7 @@ module.exports = (client) => {
             let folderName1
             try {
                 // Get folder info
-                const folderInfo = execSync(`gdrive info ${fileID}`)
+                const folderInfo = execSync(`gdrive files info ${fileID}`)
                 if (folderInfo) {
                     const infoOutput = Buffer.from(folderInfo, 'utf-8').toString()
                     console.log(infoOutput)
@@ -54,16 +54,16 @@ module.exports = (client) => {
             let outputPAth
             let StitchPath
             // Setup paths
-            if(folderName1.includes(" ")){
+            if (folderName1.includes(" ")) {
                 outputPAth = path.join(__dirname, `./downloads/${userID}/${folderName.replace(/ /g, "_")}/"${folderName1}"/"${folderName1}_Stitched"/`)
                 console.log(outputPAth)
                 StitchPath = path.join(__dirname, `./downloads/${userID}/${folderName.replace(/ /g, "_")}/"${folderName1}"`)
-            }else{
+            } else {
                 outputPAth = path.join(__dirname, `./downloads/${userID}/${folderName}/${folderName1}/${folderName1}_Stitched`)
                 StitchPath = path.join(__dirname, `./downloads/${userID}/${folderName}/${folderName1}`)
             }
             const Dwnloadpath = path.join(__dirname, `./downloads/${userID}/${folderName.replace(/ /g, "_")}`)
-            
+
             // Construct SmartStitch command based on processing options
             let stitchCmd = `SmartStitch -i ${StitchPath} -sh ${height} -t .${format}`;
             if (width != null) {
@@ -74,7 +74,7 @@ module.exports = (client) => {
             const workflow = [
                 {
                     name: 'Download',
-                    command: `gdrive files download ${fileID} --force --recursive --path ${Dwnloadpath} --skip`, 
+                    command: `gdrive files download ${fileID} --force --recursive --path ${Dwnloadpath} --skip`,
                 },
                 {
                     name: 'Stitch',
@@ -82,18 +82,18 @@ module.exports = (client) => {
                 },
                 {
                     name: 'Zip',
-                    command: `zip -r chapter-${folderName1.replace(/ /g, "_")}.zip *`, 
+                    command: `zip -r chapter-${folderName1.replace(/ /g, "_")}.zip *`,
                     options: {
                         cwd: outputPAth
                     }
                 },
                 {
                     name: 'Upload',
-                    command: `rclone copy ${outputPAth} Golden1:/stitched_BOT/${folderName1.replace(/ /g, "_")}_Stitched`,  
+                    command: `rclone copy ${outputPAth} Golden1:/stitched_BOT/${folderName1.replace(/ /g, "_")}_Stitched`,
                 },
                 {
                     name: 'Link',
-                    command : `rclone link Golden1:/stitched_BOT/${folderName1.replace(/ /g, "_")}_Stitched`,
+                    command: `rclone link Golden1:/stitched_BOT/${folderName1.replace(/ /g, "_")}_Stitched`,
                     isResult: true
                 }
             ];
@@ -102,9 +102,9 @@ module.exports = (client) => {
                 // Execute commands sequentially
                 for (const step of workflow) {
                     console.log(`[${step.name}] Executing: ${step.command}`);
-                    
+
                     const { stdout, stderr } = await execPromise(step.command, step.options || {});
-                    
+
                     if (stdout) console.log(`[${step.name}] stdout: ${stdout}`);
                     if (stderr) console.log(`[${step.name}] stderr: ${stderr}`);
 
